52417452a071cc1549cb25ec4fe6647d90b31da2
KenTong <motestt59642@gmail.com>
2016-03-24 13:16:17 +0800

    0.0

--- app/src/main/java/com/opro/ken/color_picker/MainActivity.java
+++ app/src/main/java/com/opro/ken/color_picker/MainActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-private static final int Select_Color_Request = 0;
+    private static final int Select_Color_Request = 0;
 
 
     @Override
@@ -15,15 +16,34 @@ private static final int Select_Color_Request = 0;
         setContentView(R.layout.activity_main);
     }
 
-    public void selectcolor(View view){
-        Intent intent = new Intent(this,ColorPicker.class);
+    public void selectcolor(View view) {
+        Intent intent = new Intent(this, ColorPicker.class);
         startActivityForResult(intent, Select_Color_Request);
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == Select_Color_Request){
 
+    @Override
+    protected void onActivityResult(int requestCode , int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Select_Color_Request) {
+            if (resultCode == RESULT_OK) {
+                Bundle bundle = data.getExtras();
+                int colorInt = bundle.getInt(ColorPicker.Bundle_Key_Color_Int);
+                //CharSequence colorName = bundle.getCharSequence(ColorPicker.Bundle_Key_Color_Name);
+                CharSequence insertString = bundle.getCharSequence(insert_text.Bundle_Key_String);
+
+                TextView colorpicker = (TextView) findViewById(R.id.colorpicker);
+                //colorpicker.setText(colorName);
+                colorpicker.setText(insertString);
+                colorpicker.setBackgroundColor(colorInt);
+            }
         }
+    }
+    public void next(View view) {
+        Intent intent = new Intent(this, no_1.class);
+        startActivity(intent);
+    }
+    public void insert(View view) {
+        Intent intent = new Intent(this, insert_text.class);
+        startActivity(intent);
     }
 }
